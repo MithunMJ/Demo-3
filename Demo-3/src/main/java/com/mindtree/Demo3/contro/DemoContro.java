1b2fc821abc662935c703b8b27128ddd551244e6
mindc1jun69 <mindc1jun69@c1jun158.iiht.tech>
2022-08-10 20:09:18 +0530

    test it

---
+++ Demo-3/src/main/java/com/mindtree/Demo3/contro/DemoContro.java
@@ -0,0 +1,13 @@
+package com.mindtree.Demo3.contro;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoContro 
+{
+	public String Hello()
+	{
+		return "Mithun";
+	}
+
+}
